98c21f90a5be86d937fe97271a0b976bb1da4fd8
Alexey <valnistuy@gmail.com>
2020-01-14 19:22:04 +0200

    added color-switch scripts

--- js/color-switch.js
+++ js/color-switch.js
@@ -10,3 +10,32 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const refs = {
+  bodyContent: document.querySelector("body"),
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  isActive: false,
+  colorGenerate: null
+};
+
+refs.startBtn.addEventListener("click", handleStartColorGenerateClick);
+refs.stopBtn.addEventListener("click", handleStopColorGenerateClick);
+
+function handleStartColorGenerateClick() {
+  if (refs.isActive) {
+    return;
+  }
+
+  refs.colorGenerate = setInterval(() => {
+    refs.isActive = true;
+
+    const colorRandom = randomIntegerFromInterval(0, colors.length);
+    refs.bodyContent.style.backgroundColor = colors[colorRandom];
+  }, 1000);
+}
+
+function handleStopColorGenerateClick() {
+  clearInterval(refs.colorGenerate);
+  refs.isActive = false;
+}
